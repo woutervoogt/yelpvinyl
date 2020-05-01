4992298f4b22b9d933536aebaea4ac50d908a0e4
woutervoogt <woutervoogt@gmail.com>
2020-05-01 14:14:17 +0200

    improve hamburger menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,20 @@
 const menuButton = document.getElementById("hamburger-btn");
 const menu = document.querySelector(".nav-btns");
 
-menuButton.addEventListener("click", menuButtonToggle, false);
+menuButton.addEventListener("click", menuButtonToggle, true);
 
 
 function menuButtonToggle() {
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
+    if (menu.style.display !== "block") {
       menu.style.display = "block";
       document.addEventListener("click", hideMenu, true);
     }
-  } 
+} 
   
-  function hideMenu(){
-      if(menu.style.display !== "none" && event.target.className !== "menu"  && event.target.className !== "menu-item"){
-        menu.style.display = "none";
-        document.removeEventListener("click", hideMenu, true);
-     }
-  }
+function hideMenu(e){
+    if(menu.style.display !== "none"){
+      e.stopPropagation();
+      menu.style.display = "none";
+      document.removeEventListener("click", hideMenu, true);
+    }
+}
